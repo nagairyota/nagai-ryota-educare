63cd041e6f621088533e9d6016e95b9841b3c6be
ryota nagai <4ryo3600@gmail.com>
2025-04-02 17:39:01 +0900

    Update Main2.java

--- 2-10/src/Main2.java
+++ 2-10/src/Main2.java
@@ -4,7 +4,7 @@ public class Main2 {
 
         if(score < 0 && 100 < score){
             System.out.println("無効な数字です。");
-        } else if(score >= 60){
+        } else if(score >= 60 && score <= 100){
             System.out.println("合格");
         } else {
             System.out.println("不合格");
