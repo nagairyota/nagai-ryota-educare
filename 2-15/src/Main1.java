8c8b64d21c7755fe76351bdf0953023d9e6b41b1
ryota nagai <4ryo3600@gmail.com>
2025-04-03 10:09:23 +0900

    Main1.java

--- 2-15/src/Main1.java
+++ 2-15/src/Main1.java
@@ -3,7 +3,7 @@ public class Main1 {
         int[] scores = {85,90,75,95,80};
 
         for (int i = 0; i < scores.length; i++) {
-            System.out.println(i + 1 + "番目の生徒:" + scores[i]);
+            System.out.println(i + 1 + "番目の生徒:" + scores[i] + "点");
         }
     }
 }
