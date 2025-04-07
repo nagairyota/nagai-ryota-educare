304a8edaca1df364711c2c347d2df039f427fb4c
ryota nagai <4ryo3600@gmail.com>
2025-04-07 15:58:27 +0900

    Update Main.java

--- 2-22/src/Main.java
+++ 2-22/src/Main.java
@@ -8,22 +8,39 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<String[]> wordList = null;
+        ArrayList<String[]> wordList = new ArrayList<>();
+        ArrayList<String> enList = new ArrayList<>();
+        ArrayList<String> jpList = new ArrayList<>();
+        int count = 0;
+        final int MAX_REGISTER = 20;
+
         while(true){
             System.out.println("以下の選択肢を表示します：");
             System.out.println("1: 単語を登録する\n" + "2: クイズを受ける\n" + "3: 終了する");
             int num = scan.nextInt();
+
             if(num < 0 || num > 3){
                 System.out.println("無効な入力です。");
                 break;
             }
+            
             switch(num){
                 case 1:
-                wordList = WordRegister.saveToWords();
+                if(count >= MAX_REGISTER){
+                    System.out.println("登録回数上限に達しました。");
+                    break;
+                }
+                String[] wordPair = WordRegister.saveToWords(enList, jpList);
+                if(wordPair != null){
+                    wordList.add(wordPair);
+                    count++;
+                    System.out.println("カウント：" + count);
+                    break;
+                }
                 break;
                 
                 case 2:
-                if(wordList != null && !wordList.isEmpty()){
+                if(!wordList.isEmpty()){
                     WordQuiz.work(wordList);
                 } else{
                     break;
