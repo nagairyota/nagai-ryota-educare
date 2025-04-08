523ef60e42c90f046c2dff7a4e67d2ab012e8ed2
ryota nagai <4ryo3600@gmail.com>
2025-04-08 13:18:37 +0900

    Update BankAccount.java

--- 3-3/src/Main3/BankAccount.java
+++ 3-3/src/Main3/BankAccount.java
@@ -10,14 +10,14 @@ public class BankAccount {
 
     public void deposit(int amount){
         this.balance += amount;
-        System.out.println(amount +"円預けました。" + "\n残高：" + this.balance);
+        System.out.println(amount +"円預けました。" + "\n残高：" + getBalance() + "円");
     }
     public void withdraw(int amount){
         if(this.balance < amount){
             System.out.println("残高が不足しています。");
         }else {
             this.balance -= amount;
-            System.out.println(amount + "円引き出しました。" + "\n残高：" + this.balance);
+            System.out.println(amount + "円引き出しました。" + "\n残高：" + getBalance() + "円");
         }
     }
 }
