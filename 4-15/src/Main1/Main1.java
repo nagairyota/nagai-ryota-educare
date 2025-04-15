61130aa864530e01d55086fe87cb4f9f46ac8f8e
ryota nagai <4ryo3600@gmail.com>
2025-04-15 16:55:44 +0900

    Update Main1.java

--- 4-15/src/Main1/Main1.java
+++ 4-15/src/Main1/Main1.java
@@ -2,8 +2,8 @@ package Main1;
 
 public class Main1{
     public static void main(String[] args) {
-        Fruit<String,String> fruit = new Fruit<>();
-        fruit.setData("りんご", "300");
+        Fruit fruit = new Fruit();
+        fruit.setData("りんご", 300);
         fruit.displayInfo(); // 実行時エラーにな
     }
 }
